ba4ef4511ea4ca200b4335b1b788c33da9e6a143
Mustafa Mutlu <mustafaalimutlu@gmail.com>
2022-08-07 21:22:10 +0300

    Refactor GlobalExcepitonHandler classes to be more robust.

--- ticketing/src/main/java/mutlu/ticketingapp/exception/GlobalExcepitonHandler.java
+++ ticketing/src/main/java/mutlu/ticketingapp/exception/GlobalExcepitonHandler.java
@@ -1,22 +1,43 @@
 package mutlu.ticketingapp.exception;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
 public class GlobalExcepitonHandler {
+    Logger log = LoggerFactory.getLogger(GlobalExcepitonHandler.class);
 
     @ExceptionHandler(AbstractTicketingException.class)
     public ResponseEntity<String> handle(AbstractTicketingException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        log.error("An exception occurred: {}", exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handle(IllegalArgumentException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.EXPECTATION_FAILED);
+        log.error("An exception occurred: {}", exception.getMessage());
+        return new ResponseEntity<>("Yanlış parametreler ile endpoint çağrıldı." +
+                exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handle(HttpMessageNotReadableException exception) {
+        log.error("An exception occurred: {}", exception.getMessage());
+        return new ResponseEntity<>("JSON girdisi işlenemedi. \n" +
+                exception.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception exception) {
+        log.error("An exception occurred: {}", exception.getMessage());
+        return new ResponseEntity<>("Birşeyler ters gitti. \n", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
